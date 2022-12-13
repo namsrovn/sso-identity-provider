092aed0e9f3fb4f1569529ec0188083143140439
namthebomb <namtrk95@gmail.com>
2022-12-14 00:09:02 +0700

    update .env

--- database/config.js
+++ database/config.js
@@ -6,7 +6,11 @@ const Pool = require('pg').Pool
 // const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 const pool = new Pool({
-  url: `${process.env.PG_EXTERNAL_URL}`
+  user: `${process.env.DB_USER}`,
+  host: `${process.env.DB_HOST}`,
+  database: `${process.env.DB_DATABASE}`,
+  password: `${process.env.DB_PASSWORD}`,
+  port: `${process.env.DB_PORT}`,
 })
 
 module.exports = { pool }
