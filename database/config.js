4995f2507ea41cc161b608342dc39d121e3eaad9
namthebomb <namtrk95@gmail.com>
2022-12-13 23:55:03 +0700

    modify config

--- database/config.js
+++ database/config.js
@@ -1,16 +1,12 @@
 require('dotenv').config()
 
 const Pool = require('pg').Pool
-const isProduction = process.env.NODE_ENV === 'production'
+// const isProduction = process.env.NODE_ENV === 'production'
 
-const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+// const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: {
-    rejectUnauthorized: false
-  }
-  // ssl: process.env.DATABASE_URL ? true : false
+  url: `${process.env.PG_EXTERNAL_URL}`
 })
 
 module.exports = { pool }
